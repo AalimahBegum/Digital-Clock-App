7ec19c2deeda1a50ea524a5ed837a17c4f12c600
Aalimah Begum <abegum234@outlook.com>
2026-09-20 18:15:42 +0100

    Update Digital_clock_program.py

--- Digital_clock_program.py
+++ Digital_clock_program.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
-from PyQt5.QtCore import QTimer, QTime, Qt # Qt is for alignment
+from PyQt5.QtCore import QTimer, QTime, Qt 
 
 class DigitalClock(QWidget):
     def __init__(self):
